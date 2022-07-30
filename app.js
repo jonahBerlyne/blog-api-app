aaccaba9910518ff169d8e6529a524d5f4fff5c9
jonahBerlyne <jonshaber@gmail.com>
2022-07-30 18:19:19 -0400

    added dependencies to app

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.listen(() => console.log("listening on port 3000..."));
