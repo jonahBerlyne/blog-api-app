bb3565dce146bf12b27141d72336ae539f48e38a
jonahBerlyne <jonshaber@gmail.com>
2022-09-18 12:18:40 -0400

    created setup test fn for reset password page

--- frontend/src/pages/reset_password/__tests__/ResetPassword.test.tsx
+++ frontend/src/pages/reset_password/__tests__/ResetPassword.test.tsx
@@ -4,9 +4,28 @@ import "@testing-library/jest-dom/extend-expect";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as ReactRouter from 'react-router';
 import * as ReduxHooks from "../../../redux/hooks";
-import { Provider } from "react-redux";
-import configureMockStore from "redux-mock-store";
-import thunk from 'redux-thunk';
 import ResetPasswordPage from "../[slug]";
 
-describe("Reset Password Page", () => {});
+describe("Reset Password Page", () => {
+ afterEach(() => {
+  jest.resetAllMocks();
+ });
+
+ const setup = () => {
+  jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ slug: 'example' });
+
+  const dispatch = jest.fn();
+  const useAppDispatch = jest.spyOn(ReduxHooks, 'useAppDispatch').mockReturnValue(dispatch);
+
+  const { container } = render(
+   <Router>
+    <ResetPasswordPage />
+   </Router>
+  );
+
+  return {
+   container,
+   useAppDispatch
+  };
+ }
+});
