5e9992ef7d2c8a00a0b554a1a15a24185c5effa9
jonahBerlyne <jonshaber@gmail.com>
2022-09-21 20:08:29 -0400

    added testing file for update blog page

---
+++ frontend/src/pages/update_blog/__tests__/UpdateBlog.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter as Router } from "react-router-dom";
+import * as ReactRouter from 'react-router';
+import * as ReduxHooks from "../../../redux/hooks";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import thunk from 'redux-thunk';
+import UpdateBlogPage from "../[slug]";
+import axios from "axios";
+import { act } from "react-dom/test-utils";
+import userEvent from "@testing-library/user-event";
+
+describe("Update Blog Page", () => {});
