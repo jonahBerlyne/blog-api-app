890b4e36d3cc5d67a72901dd9250ea6c28f29aa0
jonahBerlyne <jonshaber@gmail.com>
2022-09-22 06:32:12 -0400

    added setup fn for update blog test file

--- frontend/src/pages/update_blog/__tests__/UpdateBlog.test.tsx
+++ frontend/src/pages/update_blog/__tests__/UpdateBlog.test.tsx
@@ -12,4 +12,36 @@ import axios from "axios";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
 
-describe("Update Blog Page", () => {});
+describe("Update Blog Page", () => {
+ afterEach(() => {
+  jest.resetAllMocks();
+ });
+
+ const setup = async () => {
+  jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ slug: '0' });
+
+  const dispatch = jest.fn();
+  const useAppDispatch = jest.spyOn(ReduxHooks, 'useAppDispatch').mockReturnValue(dispatch);
+
+  const mockStore = configureMockStore([thunk]);
+  const store = mockStore({});
+
+  const { container } = render(
+   <Provider store={store}>
+    <Router>
+     <UpdateBlogPage />
+    </Router>
+   </Provider>
+  );
+
+  const resolve = Promise.resolve();
+  await act(async () => {
+   await resolve;
+  });
+
+  return {
+   container,
+   useAppDispatch
+  };
+ }
+});
