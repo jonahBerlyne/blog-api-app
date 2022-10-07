090548828cc0f020ef21f2844207d96933dc204c
jonahBerlyne <jonshaber@gmail.com>
2022-10-07 12:04:14 -0400

    added testing suite for api fns

--- frontend/src/utils/__tests__/Utils.test.ts
+++ frontend/src/utils/__tests__/Utils.test.ts
@@ -2,3 +2,5 @@ import "@testing-library/jest-dom/extend-expect";
 import { checkTokenExp } from "../checkTokenExp";
 
 describe("Check Token Exp Function", () => {});
+
+describe("API Functions", () => {});
