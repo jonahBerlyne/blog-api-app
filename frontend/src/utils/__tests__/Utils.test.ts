3816ca9c929f2676c09f3f988d0a9f61fa5d65c7
jonahBerlyne <jonshaber@gmail.com>
2022-10-15 01:02:56 -0400

    added check img fn to utils test file

--- frontend/src/utils/__tests__/Utils.test.ts
+++ frontend/src/utils/__tests__/Utils.test.ts
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom/extend-expect";
 import { checkTokenExp } from "../checkTokenExp";
 import { postAPI, getAPI, patchAPI, deleteAPI, putAPI } from "../FetchData";
+import { checkImg } from "../imgValidators";
 
 describe("Check Token Exp Function", () => {});
 
