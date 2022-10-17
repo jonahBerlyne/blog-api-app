8588fe028db634e97b002fc0fb7d5a2d5fcd32a0
jonahBerlyne <jonshaber@gmail.com>
2022-10-17 16:26:02 -0400

    added valid register fn to utils test file

--- frontend/src/utils/__tests__/Utils.test.ts
+++ frontend/src/utils/__tests__/Utils.test.ts
@@ -2,9 +2,10 @@ import "@testing-library/jest-dom/extend-expect";
 import { checkTokenExp } from "../checkTokenExp";
 import { postAPI, getAPI, patchAPI, deleteAPI, putAPI } from "../FetchData";
 import { checkImg, uploadImg } from "../imgValidators";
+import { validRegister } from "../Validator";
 
 describe("Check Token Exp Function", () => {});
 
 describe("API Functions", () => {});
 
-describe("Image Validators", () => {});
+describe("Validators", () => {});
