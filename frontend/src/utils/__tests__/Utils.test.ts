58ed36086e5a28f5cd4591a33009b7a7fffe393b
jonahBerlyne <jonshaber@gmail.com>
2022-10-26 11:36:48 -0400

    added valid create blog fn to utils test file

--- frontend/src/utils/__tests__/Utils.test.ts
+++ frontend/src/utils/__tests__/Utils.test.ts
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { checkTokenExp } from "../checkTokenExp";
 import { postAPI, getAPI, patchAPI, deleteAPI, putAPI } from "../FetchData";
 import { checkImg, uploadImg } from "../imgValidators";
-import { validRegister, validPhone, validEmail, checkPassword } from "../Validator";
+import { validRegister, validPhone, validEmail, checkPassword, validCreateBlog } from "../Validator";
 
 describe("Check Token Exp Function", () => {});
 
